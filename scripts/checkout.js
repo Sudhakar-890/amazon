c02da347c5d70700bc6d6d924ccd5c0da34826d0
Sudhakar-890 <sudhakarsuresh4321@gmail.com>
2026-02-07 10:42:27 +0530

    changed the fflex to grid perfectly. then also completed the lefft section of the checkout page perfectly with update and delete buttons

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,16 @@
-import {cart,store,toNewCart} from './cart.js';
-import UpdatedeliveryTime,{deliveryDate} from './delivery.js';
+import { cart, store, toNewCart } from './cart.js';
+import UpdatedeliveryTime, { deliveryDate } from './delivery.js';
 
 refreshPage()
 
-function refreshPage(){
-updateCartHTML();
+function refreshPage() {
+	updateCartHTML();
 
-function updateCartHTML()
-{
-    let html = '';
-    document.querySelector('#cart-length').innerText=cart.length;
-    cart.forEach((carts)=>{
-    html+=`  
+	function updateCartHTML() {
+		let html = '';
+		document.querySelector('#cart-length').innerText = cart.length;
+		cart.forEach((carts) => {
+			html += `  
         <div class="cart-product-box del-${carts.productId}">
             <h4 class="delivery-date delivery-date-${carts.productId}">Delivery Date : ${deliveryDate(carts)}</h4>
             <div class="cart-product">
@@ -20,10 +19,13 @@ function updateCartHTML()
                     <div>
                         <p class="product-name">${carts.productName}</p>
                         <p class="product-price">&#8377; ${carts.productPrice}</p>
-                        <p class="quantity">Quantity :<p class='count-${carts.productId} count'> ${carts.quantity}</p> 
-                            <span style="color:blue">&nbsp;&nbsp;&nbsp;&nbsp;<p class='update'>Update</p> &nbsp;&nbsp; <p class='del' data-product-id='${carts.productId}'>Delete</p> 
-                            </span>
-                            </p>
+                        <p class="quantity">Quantity :<span class='count-${carts.productId} count'> ${carts.quantity}</span><input type='number' class='input-count' data-product-id='${carts.productId}'>
+                        </p>
+                        <p class='buttons'>
+                            <p class='update' data-product-id='${carts.productId}'>Update</p>
+                            &nbsp;&nbsp; 
+							<p class='del' data-product-id='${carts.productId}'>Delete</p>   
+                        </p>
                     </div>
                 </div>
                 <div class="delivery-option-box">
@@ -33,35 +35,82 @@ function updateCartHTML()
             </div>
         </div>
     `;
-});
+		});
 
 
-const cartBox = document.querySelector('.left-section-center');
-cartBox.innerHTML=html;
-}
+		const cartBox = document.querySelector('.left-section-center');
+		cartBox.innerHTML = html;
+	}
 
-document.querySelectorAll('.delivery-times').forEach((radio)=>{
-    radio.addEventListener('change',()=>{
-        const {deliveryOption,productId} = radio.dataset
-        cart.forEach((carts)=>{
-            if(productId==carts.productId){
-                carts.options = deliveryOption;
-                store();
-                refreshPage();
-            }
-        });     
-    });
-})
+	document.querySelectorAll('.delivery-times').forEach((radio) => {
+		radio.addEventListener('change', () => {
+			const { deliveryOption, productId } = radio.dataset
+			cart.forEach((carts) => {
+				if (productId == carts.productId) {
+					carts.options = deliveryOption;
+					store();
+					refreshPage();
+				}
+			});
+		});
+	})
 
-document.querySelectorAll('.del').forEach((del)=>{
-    del.addEventListener('click',()=>{
-        const productId = del.dataset.productId;
-        let newCart = cart.filter((carts)=>
-            productId!=carts.productId
-        );
-        toNewCart(newCart);
-        refreshPage();
-    });
+	document.querySelectorAll('.del').forEach((del) => {
+		del.addEventListener('click', () => {
+			const productId = del.dataset.productId;
+			let newCart = cart.filter((carts) =>
+				productId != carts.productId
+			);
+			toNewCart(newCart);
+			refreshPage();
+		});
+	});
+
+
+const inputCount = document.querySelectorAll('.input-count');
+const update = document.querySelectorAll('.update');
+
+update.forEach((updateBtn) => {
+	updateBtn.addEventListener('click', () => {
+		if(updateBtn.innerText=='Update'){
+			const productId1 = updateBtn.dataset.productId;
+			updateBtn.innerText = 'Save';
+			inputCount.forEach((inputCountBox) => {
+				const productId = inputCountBox.dataset.productId
+				if (productId1 == productId) {
+					const displayQuantity = document.querySelector(`.count-${productId}`);
+					inputCountBox.style.display = 'inline';
+					displayQuantity.style.display='none';
+					cart.forEach((carts)=>{
+						if(carts.productId==productId){
+							const quantity = carts.quantity;
+							inputCountBox.value = quantity;
+						}
+			 		});
+				}
+			});
+		}
+		else if(updateBtn.innerText=='Save'){
+			const productId1 = updateBtn.dataset.productId;
+			updateBtn.innerText = 'Update';
+			inputCount.forEach((inputCountBox) => {
+				const productId = inputCountBox.dataset.productId
+				if (productId1 == productId) {
+					const displayQuantity = document.querySelector(`.count-${productId}`);
+					displayQuantity.style.display='inline-block';
+					const newQuantity = inputCountBox.value;
+					inputCountBox.style.display = 'none';
+					cart.forEach((carts)=>{
+						if(carts.productId==productId){
+							carts.quantity = newQuantity;
+							displayQuantity.innerText = newQuantity;
+							store();
+						}
+			 		});
+				}
+			});
+		}
+	});
 });
 
 
